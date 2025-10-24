0449680cbf22e376670f2bfd7d8634f4f178e8d3
bgsana <bgs.ana08@gmail.com>
2025-10-24 20:31:11 -0300

    Finalização das navegações Stack e Bottom Tab

--- screens/ImcScreen.js
+++ screens/ImcScreen.js
@@ -1,9 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function ImcScreen() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  const calcularIMC = () => {
+    const p = Number(peso);
+    const a = Number(altura);
+
+    if (!p || !a) {
+      setResultado('Por favor, insira peso e altura válidos.');
+      return;
+    }
+
+    const imc = p / (a * a);
+    let classificacao = '';
+
+    if (imc < 18.5) classificacao = 'Abaixo do peso';
+    else if (imc < 25) classificacao = 'Peso normal';
+    else if (imc < 30) classificacao = 'Sobrepeso';
+    else classificacao = 'Obesidade';
+
+    setResultado(`IMC: ${imc.toFixed(2)} - ${classificacao}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>ImcScreen</Text>
+    <View style={styles.calculadora}>
+      <Text style={styles.titulo}>Calculadora de IMC</Text>
+
+      <Text>Peso (kg):</Text>
+      <TextInput
+        placeholder="Ex: 70"
+        style={styles.input}
+        value={peso}
+        keyboardType="numeric"
+        onChangeText={setPeso}
+      />
+
+      <Text>Altura (m):</Text>
+      <TextInput
+        placeholder="Ex: 1.75"
+        style={styles.input}
+        value={altura}
+        keyboardType="numeric"
+        onChangeText={setAltura}
+      />
+
+      <View style={styles.botoes}>
+        <Button title="Calcular" onPress={calcularIMC} />
+      </View>
+
+      <Text style={styles.resultado}>{resultado}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  calculadora: {
+    width: 300,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+  },
+
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    textAlign: 'center',
+    color: '#7a4fc3',
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 8,
+    height: 40,
+  },
+
+  botoes: {
+    marginVertical: 10,
+    gap: 5,
+    color: '#7a4fc3',
+  },
+
+  resultado: {
+    marginTop: 15,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
